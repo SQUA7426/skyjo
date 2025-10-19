4dd949225009063d0bf5b20a494e2d041612da38
Magnum-magnus <nicholas-2@web.de>
2025-10-19 14:03:55 +0200

    Add files via upload

---
+++ Main.scala
@@ -0,0 +1,24 @@
+package de.htwg.se
+
+
+object main {
+  def main(args: Array[String]): Unit = {
+
+
+    val scal:Int = 5
+    val hor: String = "_"
+    val vert: String = "|"
+    val plus: String = "+"
+    val empt: String = " "
+
+
+    for(i<- 1 to 3) {
+      for (i <- 0 to scal) {
+        println(empt * scal + vert + empt * scal + vert)
+      }
+      if(i!=3) {
+        println(hor * (scal * 3))
+      }
+    }
+  }
+}
