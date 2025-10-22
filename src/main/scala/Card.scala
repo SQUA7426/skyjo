3d22d9c32979b4c3b60aa2b5cfbdf6ac4f1d8b10
Timothy <timo-drexler@online.de>
2025-10-22 17:15:35 +0200

    changed back into state two commits earlier

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -1,12 +1,1 @@
 package de.htwg.se
-
-import scalafx.beans.property.IntegerProperty
-import scalafx.beans.property.StringProperty
-
-object Card {
-  case class Card(xPos_ : Int, yPos_ : Int, symbol_ : String) {
-    val x = new IntegerProperty(this, "xPos_", xPos_)
-    val y = new IntegerProperty(this, "yPos_", yPos_)
-    val symbol = new StringProperty(this, "symbol_", symbol_)
-  }
-}
--- src/main/scala/Main.worksheet.sc
+++
@@ -1,5 +0,0 @@
-val scal:Int = 5
-val hor: String = "_"
-val vert: String = "|"
-val plus: String = "+"
-val empt: String = " "
