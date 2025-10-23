6ed9c8bfcf10068dfc097e063a9ccc5dca23c43d
Timothy <timo-drexler@online.de>
2025-10-23 13:47:36 +0200

    changed Main.scala 2nd time

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,33 +1,8 @@
 package de.htwg.se
 
-//
-//aaaaaaaaaaaaaaaaaaaaaaaa
-object main {
-  def main(args: Array[String]): Unit = {
-
-
-    val scal:Int = 5
-    val hor: String = "_"
-    val vert: String = "|"
-    val plus: String = "+"
-    val empt: String = " "
-
-
-    for(i<- 1 to 3) {
-      for (i <- 0 to scal) {
-        println(empt * scal + vert + empt * scal + vert)
-      }
-      if(i!=3) {
-        println(hor * (scal * 3))
-      }
-    }
-  }
-}
-
 import scala.io.StdIn.readInt
 
 @main def starting(): Unit =
   println("Enter number of players:")
   val playerCount = readInt()
   println("ok")
->>>>>>> origin/main
