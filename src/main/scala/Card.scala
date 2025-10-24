66c497746f6f40cd634b02a7f4be79e1e8325558
Timothy <timo-drexler@online.de>
2025-10-24 12:12:30 +0200

    made Board.scala, Deck.scala and changed Main.scala and Card.scala

--- src/main/scala/Card.scala
+++ src/main/scala/Card.scala
@@ -1,1 +1,10 @@
 package de.htwg.se
+
+def len(x: Int): Int = x.toString().size
+
+class Card(v: Int) {
+  val value = v
+  override def toString(): String = if len(v)==2 then s"| ${v}  | " else s"|  ${v}  | "
+}
+
+def isCard(c: Any): Boolean = c match { case _: Card => true case _ => false}
