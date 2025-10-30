7ca4fc505b6b51872b6d358aa2b1e97045228d88
Timothy <timo-drexler@online.de>
2025-10-30 11:52:36 +0100

    corrected Ticboard.scala

--- src/main/scala/de/htwg/se/Ticboard.scala
+++ src/main/scala/de/htwg/se/Ticboard.scala
@@ -1,10 +1,14 @@
 package de.htwg.se
 import scala.io.StdIn.readInt
 
-val plus: String = "+"
-val minus: String = "-"
-val sep: String = "|"
-val space: String = " "
+object TicBoard:
+  def apply(rw: Int, rlen: Int, lw: Int, lwid: Int) = {
+    require(rw > 0, "Numbers of Rows should be > 0")
+    require(rlen > 0, "Size of Row-Length should be > 0")
+    require(lw > 0, "Numbers of Columns should be > 0")
+    require(lwid > 0, "Size of Column-Length should be > 0")
+    new TicBoard(rw,rlen,lw,lwid)
+  }
 
 case class TicBoard(
     rows: Int,
@@ -12,18 +16,17 @@ case class TicBoard(
     cols: Int,
     cl: Int
 ) {
-
-  private def printRow1(): Unit =
-    for r: Int <- 0 until rows do print(plus + minus * rl * 2)
-     println(plus)
-  private def printRow2(): Unit =
-    for r2: Int <- 0 until rows do print(sep + space * rl * 2)
-     println(sep)
+  def printRow1(): Unit =
+    for r: Int <- 0 until rows do print("+" + "-" * rl * 2)
+     println("+")
+  def printRow2(): Unit =
+    for r2: Int <- 0 until rows do print("|" + " " * rl * 2)
+     println("|")
 
   def cReg(): Unit =
     for c: Int <- 0 until cols + 1 do
       printRow1()
-      if (c != cols) then for c2: Int <- 0 to cl do printRow2()
+      if (c != cols) then for c2: Int <- 0 until cl do printRow2()
 }
 
 @main def Tic1(): Unit =
@@ -35,5 +38,5 @@ case class TicBoard(
   val ll = scala.io.StdIn.readInt()
   println("Enter column-width:")
   val lw = scala.io.StdIn.readInt()
-  val t = TicBoard(rr,rh,ll,lw)
+  val t = new TicBoard(rr,rh,ll,lw)
   t.cReg()
