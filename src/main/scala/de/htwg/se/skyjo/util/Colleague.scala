564acacfb4f9714c5c07bbe77cf1517b638afd11
Timothy <timo-drexler@online.de>
2025-12-04 14:11:09 +0100

    Made testcoverage for Colleague.scala

--- src/main/scala/de/htwg/se/skyjo/util/Colleague.scala
+++ src/main/scala/de/htwg/se/skyjo/util/Colleague.scala
@@ -5,5 +5,5 @@ import de.htwg.se.skyjo.util.Mediator
 trait Colleague {
   val _mediator: Mediator
   def send(msg: String): Unit = { _mediator.send(this, msg) }
-  def receive(msg: String): Boolean = { println(s"Message received: ${msg}"); true }
+  def receive(msg: String): Boolean
 }
