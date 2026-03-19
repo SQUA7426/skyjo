c6c036fd473eeaa6f1b04fb9ae730948abbc9a72
Timothy <timo-drexler@online.de>
2026-03-19 19:01:40 +0100

    checking sbt coverage tests #2

--- src/test/scala/de/htwg/se/skyjo/aView/TuiTest.scala
+++ src/test/scala/de/htwg/se/skyjo/aView/TuiTest.scala
@@ -19,7 +19,7 @@ import java.io.ByteArrayInputStream
 import com.google.inject.{Guice, Inject, Injector}
 import de.htwg.se.skyjo.SkyjoModule
 import net.codingwell.scalaguice.InjectorExtensions.*
-import de.htwg.se.skyjo.util.utilComponent.{SupportCommand, LastHandler, LoadSaveCommand}
+import de.htwg.se.skyjo.util.utilComponent.{SupportCommand, LastHandler, LoadSaveCommand, DeckHandler, DiscHandler, SwitchHandler}
 
 class TuiTest extends AnyWordSpec with Matchers {
   "A Tui " when {
@@ -35,6 +35,12 @@ class TuiTest extends AnyWordSpec with Matchers {
       "execute an unsigned input" in:
         val cmd = new SupportCommand(ctr, ctr.getBrds(0), ctr.getDeck, ctr.getDisc)
         cmd.execute("last")
+      "handle a drawDeck" in:
+        val deH = new DeckHandler(ctr, ctr.getBrds(0), ctr.getDeck, ctr.getDisc).handle("1",0)
+      "handle a discInput" in:
+        val diH = new DiscHandler(ctr, ctr.getBrds(0), ctr.getDeck, ctr.getDisc).handle("0",0)
+      "handle a Switch" in:
+        val sH = new SwitchHandler(ctr, ctr.getBrds(0), Deck(ctr.getDeckCards, ctr.getDeck.turnUpperCard), ctr.getDisc).handle("s", 0)
       "handle an unsigned input" in:
         val lh = new LastHandler(ctr).handle("last",0)
 
@@ -60,7 +66,7 @@ class TuiTest extends AnyWordSpec with Matchers {
         }
 
       "process an Switch-Input" in:
-        val simulatedInput = "1\ns\n1\nquit\n"
+        val simulatedInput = "1\ns\n0\nquit\n"
         val in = new ByteArrayInputStream(simulatedInput.getBytes())
         Console.withIn(in) {
           tui.startGame
